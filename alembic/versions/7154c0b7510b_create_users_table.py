3c999968e95c905fdee6b345e04388dc51ff3ea6
BBeman <barbodbemanzadeh13@gmail.com>
2026-05-21 15:12:42 +0100

    Add User model and database migrations

---
+++ alembic/versions/7154c0b7510b_create_users_table.py
@@ -0,0 +1,34 @@
+"""create_users_table
+
+Revision ID: 7154c0b7510b
+Revises:
+Create Date: 2026-05-21 15:11:31.082167
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '7154c0b7510b'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('email', sa.String(length=255), nullable=False),
+        sa.Column('hashed_password', sa.String(length=255), nullable=False),
+        sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+    )
+    op.create_index('ix_users_email', 'users', ['email'], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index('ix_users_email', table_name='users')
+    op.drop_table('users')
